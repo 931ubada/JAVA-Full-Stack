638a69966d97cd134f867c0040abbf2e89d70dd6
Syed Faiz Mohammmad <syedfaizmohammadfaiz72@gmail.com>
2025-06-30 12:34:09 +0530

    final commit

--- src/array.java
+++ src/array.java
@@ -2,6 +2,7 @@ public class array {
     public static void main(String[] args) {
         char name[]={'U','b','a','d','a'};
         name[0]='m';
+        name[1]='d';
         for(int i=0; i< name.length; i++)
             System.out.print(name[i]);
     }
